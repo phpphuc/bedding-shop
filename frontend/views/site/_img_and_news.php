39550ab2980058a6de4c886806498779ff6c1b9d
phpphuc <19522036@gmail.com>
2024-08-24 11:34:36 +0700

    clear code

--- frontend/views/site/_img_and_news.php
+++ frontend/views/site/_img_and_news.php
@@ -44,10 +44,6 @@ use backend\models\Posts;
               <div class="title-decor"></div>
             </div>
             <div class="gap-6 lg:flex">
-              <!-- <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 padding-left-0">
-                                        <div class="tieude_sp"><span></span></div>
-                                        <div class="row">
-                                            <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6"> -->
               <?php
               if ($newOne) {
                 $nameOne = $newOne['name_' . Yii::$app->language];
@@ -69,31 +65,15 @@ use backend\models\Posts;
                   <div class="mt-4">
                     <a href="<?= $linkOne ?>" class="border-2 border-[#333] bg-white px-4 py-3 font-bold text-[#333]">XEM THÊM</a>
                   </div>
-                </div><?php
-                      /*
-                                ?>
-                                    <div class="hover14">
-                                        <a href="<?= $linkOne; ?>" title="<?= $nameOne; ?>">
-                                            <figure><?= Html::img(Yii::$app->thumbnailer->get($imgOne, 375, 200, 50, ImageThumb::THUMBNAIL_INSET), ['alt' => $nameOne, 'class' => 'lazy', 'data-original' => Yii::$app->thumbnailer->get($imgOne, 375, 200, 50, ImageThumb::THUMBNAIL_INSET)]); ?></figure>
-                                        </a>
-                                    </div>
-                                    <div class="_nameOne"><a href="<?= $linkOne; ?>" title="<?= $nameOne; ?>"><?= $nameOne; ?></a></div>
-                                    <div class="datenew"><?= $date ?></div>
-                                    <div class="_desOne"><?= Yii::$app->MyComponent->myTruncate($desOne, 210, " ", "..."); ?></div>
+                </div>
+              <?php
+              }
+              ?>
 
-                                <?php
-                                */
-                    }
-                      ?>
-
-              <?php ?>
-              <!-- </div> -->
-              <!-- <div class="col-lg-6 col-md-6 col-sm-6 col-xs-6 padding-left-0"> -->
               <?php
               if ($newAll) {
               ?>
                 <div class="space-y-4 -translate-y-2">
-                  <!-- <ul class="_scrollvert"> -->
                   <?php
                   foreach ($newAll as $items) :
                     $linkN = Yii::$app->urlManager->createUrl('/chi-tiet-bai-viet/' . $items['slug']);
@@ -111,93 +91,20 @@ use backend\models\Posts;
                           <?= Yii::$app->MyComponent->myTruncate($desN, 100, " ", "..."); ?>
                         </p>
                       </div>
-                      <!-- </div> -->
                     </div>
-                    <?php /*?>
-                                                <li class="news-item">
-                                                    <div class="row">
-                                                        <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 padding-left-0 padding-right-0">
-                                                            <div class="hover15"><a href="<?= $linkN; ?>" title="<?= $nameN; ?>">
-                                                                    <figure><?= Html::img(Yii::$app->thumbnailer->get($imgN, 130, 115, 50, ImageThumb::THUMBNAIL_INSET), ['alt' => $nameN, 'class' => 'lazy', 'data-original' => Yii::$app->thumbnailer->get($imgN, 130, 115, 50, ImageThumb::THUMBNAIL_INSET)]); ?></figure>
-                                                                </a></div>
-                                                        </div>
-                                                        <div class="col-lg-8 col-md-8 col-sm-8 col-xs-8 padding-left-10">
-                                                            <div class="_nameAll"><a href="<?= $linkN; ?>" title="<?= $nameN; ?>"><?= $nameN; ?></a></div>
-                                                            <div class="_desAll"><?= Yii::$app->MyComponent->myTruncate($desN, 100, " ", "..."); ?></div>
-                                                        </div>
-                                                    </div>
-                                                    <div class="clearfix margin-bottom-20"></div>
-                                                </li>
-                                                <?php */ ?>
                   <?php
                   endforeach;
                   ?>
-                  <!-- </ul> -->
                 </div>
               <?php
               }
               ?>
-              <!-- </div> -->
-              <!-- </div> -->
-              <!-- </div> -->
             </div>
           </div>
         </div>
-        <!-- </div> -->
       <?php
     }
       ?>
-      <div class="col-lg-4 col-md-4 col-sm-4 col-xs-4 padding-left-0">
-
-
-        <?php
-        if (!$duannoibat) {
-
-          $nameDM = $duannoibat['name_' . Yii::$app->language];
-          $modelCategory = (new Postscat())->getCateChild($duannoibat['id']);
-          if ($modelCategory) {
-            $duanOne = Posts::find()->where('status = 1 and feature = 1 and parent IN (' . implode(',', $modelCategory) . ')')->orderBy('number asc, id desc')->all();
-          } else {
-            $duanOne = Posts::find()->where('status = 1 and feature = 1 and parent = ' . $duannoibat['id'])->orderBy('number asc, id desc')->all();
-          }
-        ?>
-          <div class="tieude_duan"><span><?= Yii::t('app', 'Outstanding project') ?></span></div>
-          <div class="owl-carousel owl-theme owl-carousel1-brands">
-            <?php
-            if ($duanOne) {
-              foreach ($duanOne as $keyP => $valueP) {
-                $nameOne = $valueP['name_' . Yii::$app->language];
-                $desOne = $valueP['des_' . Yii::$app->language];
-                $linkOne = Yii::$app->urlManager->createUrl('/chi-tiet-bai-viet/' . $valueP['slug']);
-                $date = date_format(date_create($valueP['created_date']), 'd/m/Y');
-                if ($valueP['image']) {
-                  $imgOne = $valueP['image'];
-                } else {
-                  $imgOne = '/frontend/web/desktop/images/no-img.jpg';
-                }
-            ?>
-
-
-                <div class="hinh_duan>
-                                    st
-                                        <a href=" <?= $linkOne; ?>" title="<?= $nameOne; ?>"> <?= Html::img(Yii::$app->thumbnailer->get($imgOne, 380, 280, 50, ImageThumb::THUMBNAIL_INSET), ['alt' => $nameOne, 'class' => 'lazy', 'data-original' => Yii::$app->thumbnailer->get($imgOne, 380, 280, 50, ImageThumb::THUMBNAIL_INSET)]); ?></a>
-                  <!--     <div class="xemthem_da"><a href="<?= $linkOne ?>"><//?= Yii::t('app', 'Read more') ?></a></div> -->
-
-
-                  <div class="_nameDv"><a href="<?= $linkOne ?>" title="<?= $nameOne; ?>" style="font: 14px 'AvertaStdCY-Bold';"><span><?= $nameOne; ?></span></a></div>
-                  <div class="_btnab"><a href="<?= $linkOne ?>" title="xem thêm dự án">Xem thêm</a></div>
-
-                </div>
-
-
-            <?php }
-            } ?>
-          </div>
-        <?php
-        } ?>
-      </div>
       </div>
   </div>
-
-
 </div>
